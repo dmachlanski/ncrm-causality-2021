c31d8f9e872f20bee213f546aa145877974e02de
dmachlanski <damian.machlanski@gmail.com>
2021-06-14 20:55:11 +0100

    Upload more data for day 1 labs.

---
+++ labs/day_1/spiral_generate.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def twospirals(n_points, noise=.5):
+    """
+     Returns the two spirals dataset.
+    """
+    n = np.sqrt(np.random.rand(n_points,1)) * 780 * (2*np.pi)/360
+    d1x = -np.cos(n)*n + np.random.rand(n_points,1) * noise
+    d1y = np.sin(n)*n + np.random.rand(n_points,1) * noise
+    return (np.vstack((np.hstack((d1x,d1y)),np.hstack((-d1x,-d1y)))),
+            np.hstack((np.zeros(n_points),np.ones(n_points))))
+
+if __name__ == "__main__":
+    x, y = twospirals(1000)
+    np.savez('spirals', x=x, y=y)
